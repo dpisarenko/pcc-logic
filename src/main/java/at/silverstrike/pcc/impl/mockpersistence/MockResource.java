0b69f829b09cc128b9d69ebcfbf3710a453d0480
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-15 10:05:09 +0200

    Attempt at fixing test case failure.

---
+++ src/main/java/at/silverstrike/pcc/impl/mockpersistence/MockResource.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.impl.mockpersistence;
+
+import at.silverstrike.pcc.api.model.Resource;
+
+class MockResource implements Resource {
+    private String abbreviation;
+    private double dailyLimitInHours;
+    private Long id;
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public double getDailyLimitInHours() {
+        return dailyLimitInHours;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setAbbreviation(final String aAbbreviation) {
+        this.abbreviation = aAbbreviation;
+    }
+
+    public void setDailyLimitInHours(final double aDailyLimitInHours) {
+        this.dailyLimitInHours = aDailyLimitInHours;
+    }
+
+    public void setId(final Long aId) {
+        this.id = aId;
+    }
+
+}
