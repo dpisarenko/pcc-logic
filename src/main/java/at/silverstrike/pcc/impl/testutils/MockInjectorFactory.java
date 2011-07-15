ddf830a70d1927253432272747c01bb6b7d9285c
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-15 09:51:52 +0200

    Fixed some compiler errors.

---
+++ src/main/java/at/silverstrike/pcc/impl/testutils/MockInjectorFactory.java
@@ -0,0 +1,36 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.impl.testutils;
+
+import ru.altruix.commons.api.di.InjectorFactory;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+
+public final class MockInjectorFactory implements InjectorFactory {
+    private AbstractModule module;
+
+    public MockInjectorFactory(final AbstractModule aModule) {
+        this.module = aModule;
+    }
+
+    public Injector createInjector() {
+        final Injector injector = Guice.createInjector(this.module);
+
+        return injector;
+    }
+
+    public void setTaskJugglerPath(final String aPath) {
+    }
+}
---
+++ src/test/java/at/silverstrike/pcc/tj3deadlinesparser/MockInjectorModule.java
@@ -0,0 +1,26 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.tj3deadlinesparser;
+
+import at.silverstrike.pcc.api.embeddedfilereading.EmbeddedFileReader;
+import at.silverstrike.pcc.impl.embeddedfilereading.DefaultEmbeddedFileReaderFactory;
+
+import com.google.inject.AbstractModule;
+
+class MockInjectorModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(EmbeddedFileReader.class).toInstance(
+                new DefaultEmbeddedFileReaderFactory().create());
+    }
+}
