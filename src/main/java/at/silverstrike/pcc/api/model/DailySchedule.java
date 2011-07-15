e9e664f773ee3442ed1dbea65af56eea2c0bda14
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-15 08:57:08 +0200

    Moved api.model to pcc-logic.

---
+++ src/main/java/at/silverstrike/pcc/api/model/DailySchedule.java
@@ -0,0 +1,31 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.api.model;
+
+import java.util.List;
+
+import ru.altruix.commons.api.conventions.UniquelyIdentifiableObject;
+
+/**
+ * @author Dmitri Pisarenko
+ * 
+ */
+public interface DailySchedule extends UniquelyIdentifiableObject {
+    void setBookings(final List<Booking> aBookings);
+
+    List<Booking> getBookings();
+
+    void setUserData(final UserData aUserData);
+
+    UserData getUserData();
+
+}
