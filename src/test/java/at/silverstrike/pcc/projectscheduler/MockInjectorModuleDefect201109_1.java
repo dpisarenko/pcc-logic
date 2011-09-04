5ce192f8a4fee24e0e11c623eb4915472ba5adf1
Dmitri Pisarenko <dp@altruix.co>
2011-09-04 18:03:42 +0200

    Attempt at bugfix: ------------------------------------------------------------------------------- Test set: at.silverstrike.pcc.projectscheduler.TestDefaultProjectScheduler ------------------------------------------------------------------------------- Tests run: 3, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 48.995 sec <<< FAILURE! testDefect201109_1(at.silverstrike.pcc.projectscheduler.TestDefaultProjectScheduler)  Time elapsed: 4.284 sec  <<< ERROR! com.google.inject.ConfigurationException: Guice configuration errors:
    1) No implementation for at.silverstrike.pcc.api.gtaskrelevance.IsGoogleTaskRelevantCalculatorFactory was bound.
      while locating at.silverstrike.pcc.api.gtaskrelevance.IsGoogleTaskRelevantCalculatorFactory
    
    1 error
         at com.google.inject.InjectorImpl.getProvider(InjectorImpl.java:784)
         at com.google.inject.InjectorImpl.getProvider(InjectorImpl.java:743)
         at com.google.inject.InjectorImpl.getInstance(InjectorImpl.java:793)
         at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.getRelevantGoogleTasks(DefaultGoogleCalendarTasks2PccImporter2.java:292)
         at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.run(DefaultGoogleCalendarTasks2PccImporter2.java:77)
         at at.silverstrike.pcc.projectscheduler.TestDefaultProjectScheduler.importTasks(TestDefaultProjectScheduler.java:478)
         at at.silverstrike.pcc.projectscheduler.TestDefaultProjectScheduler.testDefect201109_1(TestDefaultProjectScheduler.java:325)
         at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
         at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)
         at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)
         at java.lang.reflect.Method.invoke(Method.java:597)
         at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:44)
         at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:15)
         at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:41)
         at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:20)
         at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:76)
         at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:50)
         at org.junit.runners.ParentRunner$3.run(ParentRunner.java:193)
         at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:52)
         at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:191)
         at org.junit.runners.ParentRunner.access$000(ParentRunner.java:42)
         at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:184)
         at org.junit.runners.ParentRunner.run(ParentRunner.java:236)
         at org.apache.maven.surefire.junit4.JUnit4TestSet.execute(JUnit4TestSet.java:62)
         at org.apache.maven.surefire.suite.AbstractDirectoryTestSuite.executeTestSet(AbstractDirectoryTestSuite.java:140)
         at org.apache.maven.surefire.suite.AbstractDirectoryTestSuite.execute(AbstractDirectoryTestSuite.java:127)
         at org.apache.maven.surefire.Surefire.run(Surefire.java:177)
         at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
         at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)
         at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)
         at java.lang.reflect.Method.invoke(Method.java:597)
         at org.apache.maven.surefire.booter.SurefireBooter.runSuitesInProcess(SurefireBooter.java:345)
         at org.apache.maven.surefire.booter.SurefireBooter.main(SurefireBooter.java:1009)

--- src/test/java/at/silverstrike/pcc/projectscheduler/MockInjectorModuleDefect201109_1.java
+++ src/test/java/at/silverstrike/pcc/projectscheduler/MockInjectorModuleDefect201109_1.java
@@ -16,6 +16,7 @@ import com.google.inject.AbstractModule;
 import at.silverstrike.pcc.api.embeddedfilereading.EmbeddedFileReader;
 import at.silverstrike.pcc.api.export2tj3.TaskJuggler3Exporter;
 import at.silverstrike.pcc.api.gcaltasks2pccimporter.GoogleCalendarTasks2PccImporter2Factory;
+import at.silverstrike.pcc.api.gtaskrelevance.IsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.api.persistence.Persistence;
 import at.silverstrike.pcc.api.projectscheduler.ProjectScheduler;
 import at.silverstrike.pcc.api.tj3bookingsparser.BookingsFile2BookingsFactory;
@@ -24,6 +25,7 @@ import at.silverstrike.pcc.api.tj3deadlinesparser.Tj3DeadlinesFileParserFactory;
 import at.silverstrike.pcc.impl.embeddedfilereading.DefaultEmbeddedFileReaderFactory;
 import at.silverstrike.pcc.impl.export2tj3.DefaultTaskJuggler3ExporterFactory;
 import at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2Factory;
+import at.silverstrike.pcc.impl.gtaskrelevance.DefaultIsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.impl.projectscheduler.DefaultProjectSchedulerFactory;
 import at.silverstrike.pcc.impl.tj3bookingsparser.DefaultBookingsFile2BookingsFactory;
 import at.silverstrike.pcc.impl.tj3bookingsparser.DefaultTj3BookingsParserFactory;
@@ -57,6 +59,8 @@ public class MockInjectorModuleDefect201109_1 extends AbstractModule {
                 toInstance(new DefaultBookingsFile2BookingsFactory());
         bind(GoogleCalendarTasks2PccImporter2Factory.class).toInstance(
                 new DefaultGoogleCalendarTasks2PccImporter2Factory());
+        bind(IsGoogleTaskRelevantCalculatorFactory.class).toInstance(
+                new DefaultIsGoogleTaskRelevantCalculatorFactory());
     }
 
 }
