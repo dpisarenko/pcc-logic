2550ae623f3ab873fb4d140eace5d9f601215384
Dmitri Pisarenko <dp@altruix.co>
2011-09-20 16:44:38 +0200

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/mockpersistence/MockDailySchedule.java
+++ src/main/java/at/silverstrike/pcc/impl/mockpersistence/MockDailySchedule.java
@@ -25,8 +25,8 @@ class MockDailySchedule implements DailySchedule {
         return user;
     }
 
-    public void setUserData(UserData user) {
-        this.user = user;
+    public void setUserData(final UserData aUser) {
+        this.user = aUser;
     }
 
     public Long getId() {
