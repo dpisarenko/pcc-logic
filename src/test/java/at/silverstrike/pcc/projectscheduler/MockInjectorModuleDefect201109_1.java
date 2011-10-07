cd4f40ff24db8816dde42efae8e6607aa2042ee0
Dmitri Pisarenko <dp@altruix.co>
2011-10-07 22:33:21 +0200

    Fixed test case failure: at.silverstrike.pcc.projectscheduler.TestDefect201109271.testDefect201109271()

--- src/test/java/at/silverstrike/pcc/projectscheduler/MockInjectorModuleDefect201109_1.java
+++ src/test/java/at/silverstrike/pcc/projectscheduler/MockInjectorModuleDefect201109_1.java
@@ -18,6 +18,7 @@ import at.silverstrike.pcc.api.export2tj3.TaskJuggler3Exporter;
 import at.silverstrike.pcc.api.gcaltasks2pccimporter.GoogleCalendarTasks2PccImporter2Factory;
 import at.silverstrike.pcc.api.gtask2pcctaskconverter.GoogleTask2PccTaskConverterFactory;
 import at.silverstrike.pcc.api.gtasknoteparser.GoogleTaskNotesParserFactory;
+import at.silverstrike.pcc.api.gtaskprioritycalculator.GoogleTasksPriorityCalculatorFactory;
 import at.silverstrike.pcc.api.gtaskrelevance.IsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.api.gtaskrelevance2.RelevantTaskSetCalculatorFactory;
 import at.silverstrike.pcc.api.gtasktitleparser.GoogleTaskTitleParserFactory;
@@ -31,6 +32,7 @@ import at.silverstrike.pcc.impl.export2tj3.DefaultTaskJuggler3ExporterFactory;
 import at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2Factory;
 import at.silverstrike.pcc.impl.gtask2pcctaskconverter.DefaultGoogleTask2PccTaskConverterFactory;
 import at.silverstrike.pcc.impl.gtasknoteparser.DefaultGoogleTaskNotesParserFactory;
+import at.silverstrike.pcc.impl.gtaskprioritycalculator.DefaultGoogleTasksPriorityCalculatorFactory;
 import at.silverstrike.pcc.impl.gtaskrelevance.DefaultIsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.impl.gtaskrelevance2.DefaultRelevantTaskSetCalculatorFactory;
 import at.silverstrike.pcc.impl.gtasktitleparser.DefaultGoogleTaskTitleParserFactory;
@@ -77,6 +79,8 @@ public final class MockInjectorModuleDefect201109_1 extends AbstractModule {
                 new DefaultGoogleTaskTitleParserFactory());
         bind(RelevantTaskSetCalculatorFactory.class).toInstance(
                 new DefaultRelevantTaskSetCalculatorFactory());
+        bind(GoogleTasksPriorityCalculatorFactory.class).toInstance(
+                new DefaultGoogleTasksPriorityCalculatorFactory());
     }
 
 }
